83a5514331ef562bb15b8cf0cf93ffcc4461b3f3
southxzx <phuongnamsk109@gmail.com>
2024-04-14 15:33:20 +0700

    add ga4 tracking

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -109,6 +109,30 @@ const config: GatsbyConfig = {
         icon: "src/images/favicon_v.png",
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: ["G-G4278L3C0R"],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
+      },
+    },
   ],
 };
 
